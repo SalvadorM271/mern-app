6d27fb593c0ffe3b778dfe231dad085ca813c6b8
salvadorM271 <guardianes27@outlook.com>
2022-11-03 11:45:15 -0600

    reset service

--- ui/src/components/Header.js
+++ ui/src/components/Header.js
@@ -5,7 +5,7 @@ const Header = () => {
     return (
         <div className="header">
             <h1>
-                MERN Stack Examplev2
+                MERN Stack Example v3
             </h1>
         </div>
     )
